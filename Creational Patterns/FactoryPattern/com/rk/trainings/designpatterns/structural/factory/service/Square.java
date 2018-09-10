782c8a9099dc6a16f0a161ef41414c1e37e6ade1
Ravikumar Pothannagari <kumar.sku85@gmail.com>
2018-09-10 14:10:44 +0530

    Square draw method sysout changed.
    Square draw method sysout changed.

--- Creational Patterns/FactoryPattern/com/rk/trainings/designpatterns/structural/factory/service/Square.java
+++ Creational Patterns/FactoryPattern/com/rk/trainings/designpatterns/structural/factory/service/Square.java
@@ -11,7 +11,7 @@ public class Square implements IShape {
 
 	@Override
 	public void draw() {
-		System.out.println("Inside Circle::draw() method.");
+		System.out.println("Inside Square::draw() method.");
 	}
 
 }
